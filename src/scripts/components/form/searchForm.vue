27c437bb4d6b6893e13b911785918c73e92e8e28
yuki-kuwano <yuki-kuwano@navitime.co.jp>
2020-02-18 13:59:33 +0900

    最低限追加

---
+++ src/scripts/components/form/searchForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <input type="text" v-model="text">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default Vue.component('search-form', {
+  data: () => ({ text: 'test' })
+});
+</script>
---
+++ task/config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  path: {
+    src: {
+      root: './src/',
+      html: './src/html/',
+      img: './src/img/',
+      scripts: {
+        lib: './src/scripts/lib/',
+        view: './src/scripts/view/'
+      },
+      styles: {
+        lib: './src/styles/lib/',
+        view: './src/styles/view/'
+      }
+    },
+    dist: {
+      root: './dist/',
+      html: './dist/html/',
+      img: './dist/img/',
+      scripts: {
+        lib: './dist/scripts/lib/',
+        view: './dist/scripts/view/'
+      },
+      styles: {
+        lib: './dist/styles/lib/',
+        view: './dist/styles/view/'
+      }
+    }
+  }
+};
