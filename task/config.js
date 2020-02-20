f58a4ffbbe2f5b70241c702caef211a65633ccd5
yuki-kuwano <yuki-kuwano@navitime.co.jp>
2020-02-20 19:14:48 +0900

    パス追加

--- task/config.js
+++ task/config.js
@@ -8,10 +8,14 @@ module.exports = {
       img: './src/img/',
       scripts: {
         lib: './src/scripts/lib/',
+        components: './src/scripts/components/',
+        infra: './src/scripts/infra/',
+        store: './src/scripts/store/',
         pages: './src/scripts/pages/'
       },
       styles: {
         lib: './src/styles/lib/',
+        common: './src/styles/common/',
         pages: './src/styles/pages/'
       }
     },
@@ -21,10 +25,14 @@ module.exports = {
       img: './dist/img/',
       scripts: {
         lib: './dist/scripts/lib/',
+        components: './dist/scripts/components/',
+        infra: './dist/scripts/infra/',
+        store: './dist/scripts/store/',
         pages: './dist/scripts/pages/'
       },
       styles: {
         lib: './dist/styles/lib/',
+        common: './dist/styles/common/',
         pages: './dist/styles/pages/'
       }
     }
